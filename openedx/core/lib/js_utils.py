7f692ded49fd0ad0322a9b7dbe49ca8b440ca752
Robert Raposa <rraposa@edx.org>
2015-12-23 19:28:25 -0500

    Use short Mako filter names for json/js
    TNL-3425

--- openedx/core/lib/js_utils.py
+++ openedx/core/lib/js_utils.py
@@ -1,7 +1,7 @@
 """
 Utilities for dealing with Javascript and JSON.
 """
-import json
+import json as jsonlib
 from django.template.defaultfilters import escapejs
 from mako.filters import decode
 from xmodule.modulestore import EdxJSONEncoder
@@ -30,16 +30,18 @@ def escape_json_dumps(obj, cls=EdxJSONEncoder):
     """
     JSON dumps and escapes JSON that is safe to be embedded in HTML.
 
-    Usage:
-        Can be used inside a Mako template inside a <SCRIPT> as follows:
-            var my_json = ${escape_json_dumps(my_object) | n}
+    NOTE: If not using the cls argument, use the json() method instead
+    as detailed in its notes.
 
-        Use the "n" Mako filter above.  It is possible that the
-            default filter may include html escaping in the future, and
-            we must make sure to get the proper escaping.
+    Usage:
+        Can be used inside a Mako template inside a <SCRIPT> as follows::
+        var my_json = ${escape_json_dumps(my_object, cls) | n}
+
+        Use the "n" Mako filter above.  It is possible that the default filter
+        may include html escaping in the future, and this ensures proper escaping.
 
         Ensure ascii in json.dumps (ensure_ascii=True) allows safe skipping of Mako's
-            default filter decode.utf8.
+        default filter decode.utf8.
 
     Arguments:
         obj: The json object to be encoded and dumped to a string
@@ -49,7 +51,7 @@ def escape_json_dumps(obj, cls=EdxJSONEncoder):
         (string) Escaped encoded JSON
 
     """
-    encoded_json = json.dumps(obj, ensure_ascii=True, cls=cls)
+    encoded_json = jsonlib.dumps(obj, ensure_ascii=True, cls=cls)
     encoded_json = _escape_json_for_html(encoded_json)
     return encoded_json
 
@@ -58,18 +60,21 @@ def escape_js_string(js_string):
     """
     Escape a javascript string that is safe to be embedded in HTML.
 
+    NOTE: If there isn't a good reason to use this more descriptive
+    method name, use the more brief js() method as documented as
+    a Mako filter.
+
     Usage:
-        Can be used inside a Mako template inside a <SCRIPT> as follows:
-            var my_js_string = "${escape_js_string(my_js_string) | n}"
+        Can be used inside a Mako template inside a <SCRIPT> as follows::
+        var my_js_string = "${escape_js_string(my_js_string) | n}"
 
         Must include the surrounding quotes for the string.
 
-        Use the "n" Mako filter above.  It is possible that the
-            default filter may include html escaping in the future, and
-            we must make sure to get the proper escaping.
+        Use the "n" Mako filter above.  It is possible that the default filter
+        may include html escaping in the future, and this ensures proper escaping.
 
         Mako's default filter decode.utf8 is applied here since this default
-            filter is skipped in the Mako template with "n".
+        filter is skipped in the Mako template with "n".
 
     Arguments:
         js_string (string): The javascript string to be escaped
@@ -81,3 +86,54 @@ def escape_js_string(js_string):
     js_string = decode.utf8(js_string)
     js_string = escapejs(js_string)
     return js_string
+
+
+def json(obj):
+    """
+    Mako filter that JSON dumps and escapes JSON that is safe to be embedded in HTML.
+
+    Usage:
+        Can be used inside a Mako template inside a <SCRIPT> as follows::
+        var my_json = ${my_object | n,json}
+
+        Use the "n" Mako filter above.  It is possible that the default filter
+        may include html escaping in the future, and this ensures proper escaping.
+
+        Ensure ascii in json.dumps (ensure_ascii=True) allows safe skipping of Mako's
+        default filter decode.utf8.
+
+    Arguments:
+        obj: The json object to be encoded and dumped to a string
+
+    Returns:
+        (string) Escaped encoded JSON
+
+    """
+    return escape_json_dumps(obj)
+
+
+# pylint: disable=invalid-name
+def js(js_string):
+    """
+    Mako filter that escapes a javascript string that is safe to be embedded in HTML.
+
+    Usage:
+        Can be used inside a Mako template inside a <SCRIPT> as follows::
+        var my_js_string = "${my_js_string) | n,js}"
+
+        Must include the surrounding quotes for the string.
+
+        Use the "n" Mako filter above.  It is possible that the default filter
+        may include html escaping in the future, and this ensures proper escaping.
+
+        Mako's default filter decode.utf8 is applied here since this default
+        filter is skipped in the Mako template with "n".
+
+    Arguments:
+        js_string (string): The javascript string to be escaped
+
+    Returns:
+        (string) Escaped javascript as unicode
+
+    """
+    return escape_js_string(js_string)
--- openedx/core/lib/tests/test_js_utils.py
+++ openedx/core/lib/tests/test_js_utils.py
@@ -1,10 +1,10 @@
 """
 Tests for js_utils.py
 """
-import json
+import json as jsonlib
 from unittest import TestCase
 from openedx.core.lib.js_utils import (
-    escape_json_dumps, escape_js_string
+    escape_json_dumps, escape_js_string, json, js
 )
 
 
@@ -20,7 +20,7 @@ class TestJSUtils(TestCase):
         def __init__(self, value):
             self.value = value
 
-    class SampleJSONEncoder(json.JSONEncoder):
+    class SampleJSONEncoder(jsonlib.JSONEncoder):
         """
         A test encoder that is used to prove that the encoder does its job before the escaping.
         """
@@ -39,6 +39,19 @@ class TestJSUtils(TestCase):
         )
 
         encoded_json = escape_json_dumps(malicious_json)
+        self.assertEquals(expected_encoded_json, encoded_json)
+
+    def test_json_escapes_unsafe_html(self):
+        """
+        Test json properly escapes &, <, and >.
+        """
+        malicious_json = {"</script><script>alert('hello, ');</script>": "</script><script>alert('&world!');</script>"}
+        expected_encoded_json = (
+            r'''{"\u003c/script\u003e\u003cscript\u003ealert('hello, ');\u003c/script\u003e": '''
+            r'''"\u003c/script\u003e\u003cscript\u003ealert('\u0026world!');\u003c/script\u003e"}'''
+        )
+
+        encoded_json = json(malicious_json)
         self.assertEquals(expected_encoded_json, encoded_json)
 
     def test_escape_json_dumps_with_custom_encoder_escapes_unsafe_html(self):
@@ -71,4 +84,16 @@ class TestJSUtils(TestCase):
             r"\u003C/script\u003E\u003Cscript\u003Ealert(\u0027hello, \u0027)\u003B\u003C/script\u003E"
         )
         escaped_js_string = escape_js_string(malicious_js_string)
+        self.assertEquals(expected_escaped_js_string, escaped_js_string)
+
+    def test_js_escapes_unsafe_html(self):
+        """
+        Test js escapes &, <, and >, as well as returns a unicode type
+        """
+        malicious_js_string = "</script><script>alert('hello, ');</script>"
+
+        expected_escaped_js_string = unicode(
+            r"\u003C/script\u003E\u003Cscript\u003Ealert(\u0027hello, \u0027)\u003B\u003C/script\u003E"
+        )
+        escaped_js_string = js(malicious_js_string)
         self.assertEquals(expected_escaped_js_string, escaped_js_string)
